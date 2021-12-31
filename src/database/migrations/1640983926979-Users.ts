510831a55078434fdad73582efc75ae294677c52
João Pedro Barbosa <joaopedrofbarbosa@protonmail.com>
2021-12-31 19:26:03 -0300

    style(1640983926979-Users.ts): fix indentation

--- src/database/migrations/1640983926979-Users.ts
+++ src/database/migrations/1640983926979-Users.ts
@@ -1,56 +1,56 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class Users1640983926979 implements MigrationInterface {
-	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.dropTable('users');
-		await queryRunner.createTable(new Table({
-			name: 'users',
-			columns: [
-				{
-					name: 'user_id',
-					type: 'uuid',
-					isPrimary: true,
-				},
-				{
-					name: 'email',
-					type: 'varchar',
-					isNullable: false,
-					isUnique: true,
-				},
-				{
-					name: 'password',
-					type: 'varchar',
-					isNullable: false,
-				},
-				{
-					name: 'name',
-					type: 'varchar',
-					isNullable: false,
-				},
-				{
-					name: 'avatar',
-					type: 'varchar',
-				},
-				{
-					name: 'authorized',
-					type: 'boolean',
-					isNullable: false,
-				},
-				{
-					name: 'created_at',
-					type: 'timestamp',
-					default: 'NOW()',
-				},
-				{
-					name: 'updated_at',
-					type: 'timestamp',
-					default: 'NOW()',
-				},
-			],
-		}));
-	}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+    await queryRunner.createTable(new Table({
+      name: 'users',
+        columns: [
+         {
+           name: 'user_id',
+           type: 'uuid',
+           isPrimary: true,
+	 },
+	 {
+           name: 'email',
+           type: 'varchar',
+           isNullable: false,
+	   isUnique: true,
+	 },
+	 {
+           name: 'password',
+           type: 'varchar',
+	   isNullable: false,
+	 },
+	 {
+           name: 'name',
+           type: 'varchar',
+           isNullable: false,
+         },
+         {
+           name: 'avatar',
+           type: 'varchar',
+         },
+         {
+           name: 'authorized',
+           type: 'boolean',
+           isNullable: false,
+         },
+         {
+           name: 'created_at',
+           type: 'timestamp',
+	   default: 'NOW()',
+         },
+	 {
+           name: 'updated_at',
+           type: 'timestamp',
+           default: 'NOW()',
+         },
+      ],
+    }));
+  }
 
-	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.dropTable('users');
-	}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
 }
