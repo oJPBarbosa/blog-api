17d0dabb2bd9a62b2f4b5f00921617303a5a1d1c
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-01 12:22:06 -0300

    style(migrations/Posts): fix indentation

--- src/database/migrations/1641049227159-Posts.ts
+++ src/database/migrations/1641049227159-Posts.ts
@@ -1,70 +1,70 @@
 import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
 
 export class Posts1641049227159 implements MigrationInterface {
-	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.createTable(new Table({
-			name: 'posts',
-			columns: [
-				{
-					name: 'post_id',
-					type: 'uuid',
-					isPrimary: true,
-				},
-				{
-					name: 'author_id',
-					type: 'uuid',
-					isNullable: false,
-					isUnique: true,
-				},
-				{
-					name: 'title',
-					type: 'varchar',
-					isNullable: false,
-				},
-				{
-					name: 'description',
-					type: 'varchar',
-					isNullable: false,
-				},
-				{
-					name: 'tags',
-					type: 'varchar',
-					isNullable: false,
-				},
-				{
-					name: 'content',
-					type: 'varchar',
-					isNullable: false,
-				},
-				{
-					name: 'created_at',
-					type: 'timestamp',
-					default: 'NOW()',
-					isNullable: false,
-				},
-				{
-					name: 'updated_at',
-					type: 'timestamp',
-					default: 'NOW()',
-					isNullable: false,
-				},
-			],
-		}));
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'posts',
+      columns: [
+        {
+          name: 'post_id',
+          type: 'uuid',
+          isPrimary: true,
+        },
+        {
+          name: 'author_id',
+          type: 'uuid',
+          isNullable: false,
+          isUnique: true,
+        },
+        {
+          name: 'title',
+          type: 'varchar',
+          isNullable: false,
+        },
+        {
+          name: 'description',
+          type: 'varchar',
+          isNullable: false,
+        },
+        {
+          name: 'tags',
+          type: 'varchar',
+          isNullable: false,
+        },
+        {
+          name: 'content',
+          type: 'varchar',
+          isNullable: false,
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'NOW()',
+          isNullable: false,
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'NOW()',
+          isNullable: false,
+        },
+      ],
+    }));
 
-		await queryRunner.createForeignKey('posts', new TableForeignKey({
-			columnNames: [
-				'author_id'
-			],
-			referencedColumnNames: [
-				'user_id'
-			],
-			referencedTableName: 'users',
-			onUpdate: 'CASCADE',
-			onDelete: 'CASCADE',
-		}));
-	}
+    await queryRunner.createForeignKey('posts', new TableForeignKey({
+      columnNames: [
+        'author_id'
+      ],
+      referencedColumnNames: [
+        'user_id'
+      ],
+      referencedTableName: 'users',
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    }));
+  }
 
-	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.dropTable('posts');
-	}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('posts');
+  }
 }
