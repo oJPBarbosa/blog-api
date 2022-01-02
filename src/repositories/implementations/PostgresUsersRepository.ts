a24cde903e6522d1f6ec8cde14619c6c097f66cd
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-01 22:27:58 -0300

    style(PostgresUsersRepository.ts): move User importation

--- src/repositories/implementations/PostgresUsersRepository.ts
+++ src/repositories/implementations/PostgresUsersRepository.ts
@@ -1,6 +1,6 @@
 import { IUsersRepository } from '../IUsersRepository'
-import { Repository, getRepository } from 'typeorm'
 import { User } from '../../entities/User'
+import { Repository, getRepository } from 'typeorm'
 
 export class PostgresUsersRepository implements IUsersRepository {
   public async findById(user_id: string): Promise<User | undefined> {
