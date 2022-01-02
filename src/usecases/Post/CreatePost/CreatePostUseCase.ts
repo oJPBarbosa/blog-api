ee3fd248f2e1661bc632a5e32f43edbe8fda6bb9
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-02 12:52:39 -0300

    feat(CreatePostUseCase.ts): ensure that user exists before creating a post

--- src/usecases/Post/CreatePost/CreatePostUseCase.ts
+++ src/usecases/Post/CreatePost/CreatePostUseCase.ts
@@ -1,16 +1,25 @@
 import { IPostsRepository } from '../../../repositories/IPostsRepository'
+import { IUsersRepository } from '../../../repositories/IUsersRepository'
 import { CreatePostRequestDTO } from './CreatePostDTO'
+import { User } from '../../../entities/User'
 import { Post } from '../../../entities/Post'
 
 export class CreatePostUseCase {
   constructor(
     private postsRepository: IPostsRepository,
+    private usersRepository: IUsersRepository,
   ) {}
 
   async execute(data: CreatePostRequestDTO): Promise<Post> {
     const { author_id, title, description, tags, content } = data;
 
-    const post = new Post({
+    const user: User = await this.usersRepository.findById(author_id);
+
+    if (!user) {
+      throw new Error('User not found.');
+    }
+
+    const post: Post = new Post({
       author_id,
       title,
       description,
