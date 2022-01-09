8fa1f8fd9598018debd06c71b83f06407a81e072
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-09 10:27:49 -0300

    refactor(api): switch from manual slugify to package slugify

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,17 +1,10 @@
+import slug from 'slugify'
+
 export const slugify = (string: string): string => {
-  string = string.replace(/^\s+|\s+$/g, '')
-    .toLowerCase();
-
-  const from = "àáäãâèéëêìíïîòóöôùúüûñç·/_,:;";
-  const to = "aaaaaeeeeiiiioooouuuunc------";
-  
-  for (let i = 0; i < from.length; i++) {
-    string = string.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-  }
-
-  string = string.replace(/[^a-z0-9 -]/g, '')
-    .replace(/\s+/g, '-')
-    .replace(/-+/g, '-');
-
-  return string;
+  return slug(string, {
+    remove: /[\|'"!@#$%¨&*()_+=[]{};:.>,<]/g,
+    lower: true,
+    strict: true,
+    trim: true,
+  })
 }
